22453a53f2162d09d25dc8e0ab8c2675ba9f54ed
Arlandis Lawrence <arlandislawrence@gmail.com>
2013-11-13 11:08:33 -0600

    Adds more tests

--- spec/n_bodies_harness/core_spec.clj
+++ spec/n_bodies_harness/core_spec.clj
@@ -13,6 +13,13 @@
     (let [state-of-world [{:mass 2 :position {:x 0 :y 3 :z 0}}]]
     (should= {:mass 2 :position {:x 0 :y 8 :z 0}} (first (time-step-universe state-of-world))))))
 
+(describe "initialize-universe!"
+  (it "stores an infinite lazy seq in an atom"
+    (let [step-fn inc
+          initial-state 1]
+    (initialize-universe! initial-state step-fn)
+    (should= 2 (second @universe-timeline)))))
+
 (describe "random-color"
   (it "picks a color from a random set"
     (let [fake-colors [[:foo] [:bar] [:baz]]]
@@ -22,6 +29,11 @@
 (describe "scale-to-size"
   (it "scales masses by the standard size"
     (should= (* STANDARD_BODY_SIZE 3) (scale-to-size 3))))
+
+(describe "add-color"
+  (it "adds a color association to a data structure"
+    (with-redefs [random-color (fn [& _] [12 11 10])]
+      (should= {:color [12 11 10] :foo "bar"} (add-color {:foo "bar"})))))
 
 (describe "draw-all-bodies"
   (xit "applies the draw-body fn to all bodies in the state"
